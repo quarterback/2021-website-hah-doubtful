a603bf002f59a4572fd36fa4f0a540380be3c0c4
Ron Bronson <ron@ronbronson.com>
2020-08-31 12:17:38 -0700

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="mailto:ron@ronbronson.com">Email me</Link>
+    <a href="mailto:ron@ronbronson.com">Email me</a>
     <ThemeChanger/>
   </nav>
   
